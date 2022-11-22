00940a0ec4aa494ed4a4e1f0916728879a95906d
Tiago Vaz <tiago.vaz@hotmail.com>
2022-11-21 22:07:38 -0300

    chore: add gorilla mux

--- app/app.go
+++ app/app.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
+	mux := mux.NewRouter()
 		// define routes
 	mux.HandleFunc("/greet", greet)
 	mux.HandleFunc("/customers", getAllCustomers)
